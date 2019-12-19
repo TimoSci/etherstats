6a9201fc2d9e4d8c7c0355ee049bebff10b55bf4
TimoSci <me@timosci.net>
2019-12-19 03:10:46 +0100

    basic index page working

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -2,7 +2,7 @@ class BlocksController < ApplicationController
 
   def index
     @latest_block = Block.latest
-    byebug
+    @latest_blocks = Block.find_latest(100)
     # @latest_block = connection.latest_block
   end
 
